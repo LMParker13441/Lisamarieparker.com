a4f924744e6f700f2b03bc79b230cbc1002ecdb7
marienaq <mrs.marienaq@gmail.com>
2024-05-15 18:18:48 -0700

    update to remove deprecated features for deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  target: 'experimental-serverless-trace',
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return defaultPathMap;
+  },
+};
